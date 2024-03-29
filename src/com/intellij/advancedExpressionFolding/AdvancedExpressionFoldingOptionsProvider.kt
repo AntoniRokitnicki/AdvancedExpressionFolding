9523623061a1e30bad20cd57fe99685608da3934
Antoni <antoni2work@gmail.com>
2024-03-29 21:54:06 +0100

    Abstract class for AbstractExpressionFoldingOptionsProvider

--- src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.kt
+++ src/com/intellij/advancedExpressionFolding/AdvancedExpressionFoldingOptionsProvider.kt
@@ -1,28 +1,6 @@
 package com.intellij.advancedExpressionFolding
 
-import com.intellij.advancedExpressionFolding.extension.asInstance
-import com.intellij.application.options.editor.CodeFoldingOptionsProvider
-import com.intellij.ide.BrowserUtil
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.options.BeanConfigurable
-import com.intellij.ui.components.ActionLink
-import java.awt.Component
-import java.awt.FlowLayout
-import java.awt.GridLayout
-import javax.swing.JCheckBox
-import javax.swing.JComponent
-import javax.swing.JPanel
-import kotlin.reflect.KMutableProperty0
-
-class AdvancedExpressionFoldingOptionsProvider protected constructor() :
-    BeanConfigurable<AdvancedExpressionFoldingSettings.State?>(
-        AdvancedExpressionFoldingSettings.getInstance().state
-    ), CodeFoldingOptionsProvider {
-
-    private val log: Logger = Logger.getInstance(AdvancedExpressionFoldingOptionsProvider::class.java)
-
-
-    private val checkboxAddonMap = HashMap<String, Component>()
+class AdvancedExpressionFoldingOptionsProvider : AbstractExpressionFoldingOptionsProvider() {
 
     init {
         val state = AdvancedExpressionFoldingSettings.getInstance().state
@@ -177,74 +155,5 @@ class AdvancedExpressionFoldingOptionsProvider protected constructor() :
         )
     }
 
-    private fun checkBox(
-        title: String,
-        property: KMutableProperty0<Boolean>,
-        exampleLinkMap: Map<UrlSuffix, Description?>? = null,
-        docLink: UrlSuffix? = null
-    ) {
-        super.checkBox(title, property)
-
-        try {
-            val panel = JPanel()
-            val gridLayout = FlowLayout(FlowLayout.LEFT)
-            panel.setLayout(gridLayout)
-            docLink?.let {
-                addLink(panel, "doc", "https://github.com/AntoniRokitnicki/AdvancedExpressionFolding/wiki$it")
-            }
-            exampleLinkMap?.let {
-                it.forEach { (file, desc) ->
-                    val description = "example" + if (desc != null) {
-                        " $desc"
-                    } else {
-                        ""
-                    }
-
-                    addLink(
-                        panel,
-                        description,
-                        "https://raw.githubusercontent.com/AntoniRokitnicki/AdvancedExpressionFolding/master/test/data/$file"
-                    )
-                }
-            }
-            checkboxAddonMap[title] = panel
-        } catch (e: Exception) {
-            log.error("Unexpected issue while creating checkBox for $title", e);
-        }
-    }
-
-    private fun addLink(panel: JPanel, title: String, url: String): Component? {
-        val actionLink = ActionLink(title) {
-            BrowserUtil.browse(url);
-        }
-        actionLink.setExternalLinkIcon()
-        return panel.add(actionLink)
-    }
-
-    override fun createComponent(): JComponent? {
-        val superComponent = super.createComponent() ?: return null
-        try {
-            val mainPanel = superComponent.components?.firstOrNull().asInstance<JPanel>() ?: return superComponent
-            val withLinks = mainPanel.components?.filterIsInstance<JCheckBox>()?.mapIndexed { index, checkBox ->
-                val text = checkBox.text
-                val link = checkboxAddonMap[text] ?: return@mapIndexed null
-                Pair(link, index + 1)
-            }?.filterNotNull()?.reversed() ?: return superComponent
-
-            withLinks.forEach { (link, index) ->
-                mainPanel.add(link, index)
-            }
-
-            (mainPanel.layout as? GridLayout)?.let {
-                it.rows += withLinks.size
-            }
-        } catch (e: Exception) {
-            log.error("Unexpected issue while creating component", e);
-        }
-        return superComponent
-    }
-
 }
 
-typealias Description = String
-typealias UrlSuffix = String
