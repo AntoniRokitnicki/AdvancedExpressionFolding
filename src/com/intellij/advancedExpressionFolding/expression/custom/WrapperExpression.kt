87febeb20d26b98d34ed3b1153f6e848a78c5a9a
Antoni <antoni2work@gmail.com>
2024-05-09 20:06:30 +0200

    same instance of folding group for DestructuringExpression & LoggerBracketExpression

--- src/com/intellij/advancedExpressionFolding/expression/custom/WrapperExpression.kt
+++ src/com/intellij/advancedExpressionFolding/expression/custom/WrapperExpression.kt
@@ -2,29 +2,13 @@ package com.intellij.advancedExpressionFolding.expression.custom
 
 import com.intellij.advancedExpressionFolding.expression.Expression
 import com.intellij.lang.folding.FoldingDescriptor
-import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.FoldingGroup
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 
-class WrapperExpression(element: PsiElement, textRange: TextRange, private val chain : List<Expression>) : Expression(element, textRange) {
-    override fun buildFoldRegions(
-        element: PsiElement,
-        document: Document,
-        parent: Expression?
-    ): Array<FoldingDescriptor> {
-        return chain.flatMap {
-            it.buildFoldRegions(it.element, document, it).toList()
-        }.toTypedArray()
-    }
+class WrapperExpression(element: PsiElement, textRange: TextRange, chain : List<Expression>) :
+    AbstractMultiExpression(element, textRange, *chain.toTypedArray()) {
 
-    override fun isCollapsedByDefault(): Boolean {
-        return true
-    }
-
-    override fun supportsFoldRegions(
-        document: Document,
-        parent: Expression?
-    ): Boolean {
-        return true
-    }
+    override fun wrapElement(element: PsiElement, parentGroup: FoldingGroup): MutableList<FoldingDescriptor> =
+        mutableListOf()
 }
