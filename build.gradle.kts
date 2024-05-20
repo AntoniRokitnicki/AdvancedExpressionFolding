644e7493f8fc3d00e974f6c44e884fd4c221c0cc
Antoni <antoni2work@gmail.com>
2024-05-20 23:55:56 +0200

    [test] junit5

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,7 @@ tasks.withType<JavaCompile> {
 tasks.test {
     systemProperty("file.encoding", "UTF-8")
     systemProperty("line.separator", "\n")
+    useJUnitPlatform()
 }
 
 // Configure project's dependencies
@@ -48,6 +49,9 @@ repositories {
 dependencies {
 //    implementation(libs.annotations)
     implementation("org.jsoup:jsoup:1.17.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.0-M2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0-M2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.0-M2")
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
@@ -109,7 +113,7 @@ tasks {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
 
-            with (it.lines()) {
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
@@ -156,7 +160,10 @@ tasks {
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels = properties("pluginVersion").map { listOf(it.substringAfterLast('-', "").substringBefore('.').ifEmpty { "default" }) }
+        channels = properties("pluginVersion").map {
+            listOf(
+                it.substringAfterLast('-', "").substringBefore('.').ifEmpty { "default" })
+        }
     }
 
     apply(from = "build-version.gradle.kts")
--- test/com/intellij/advancedExpressionFolding/PropertyUtilTest.kt
+++ test/com/intellij/advancedExpressionFolding/PropertyUtilTest.kt
@@ -1,20 +1,19 @@
-package com.intellij.advancedExpressionFolding;
+package com.intellij.advancedExpressionFolding
 
-import org.junit.Test;
+import com.intellij.advancedExpressionFolding.extension.PropertyUtil.guessPropertyName
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
-import static com.intellij.advancedExpressionFolding.extension.PropertyUtil.guessPropertyName;
-import static org.junit.Assert.assertEquals;
-
-public class PropertyUtilTest {
+class PropertyUtilTest {
     @Test
-    public void testGuessPropertyName() {
-        assertEquals("", guessPropertyName(""));
-        assertEquals("length", guessPropertyName("length"));
-        assertEquals("name", guessPropertyName("getName"));
-        assertEquals("name", guessPropertyName("setName"));
-        assertEquals("enabled", guessPropertyName("isEnabled"));
-        assertEquals("enabledByDefault", guessPropertyName("isEnabledByDefault"));
-        assertEquals("html", guessPropertyName("getHTML"));
-        assertEquals("htmlText", guessPropertyName("isHTMLText"));
+    fun testGuessPropertyName() {
+        assertEquals("", guessPropertyName(""))
+        assertEquals("length", guessPropertyName("length"))
+        assertEquals("name", guessPropertyName("getName"))
+        assertEquals("name", guessPropertyName("setName"))
+        assertEquals("enabled", guessPropertyName("isEnabled"))
+        assertEquals("enabledByDefault", guessPropertyName("isEnabledByDefault"))
+        assertEquals("html", guessPropertyName("getHTML"))
+        assertEquals("htmlText", guessPropertyName("isHTMLText"))
     }
 }
