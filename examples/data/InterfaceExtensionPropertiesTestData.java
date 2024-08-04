256d4528900892d3f82a147739f8a3d3f63c8f11
Antoni <antoni2work@gmail.com>
2024-08-04 16:28:42 +0200

    [interfaceExtensionProperties] init

--- examples/data/InterfaceExtensionPropertiesTestData.java
+++ examples/data/InterfaceExtensionPropertiesTestData.java
@@ -10,23 +10,217 @@ public class InterfaceExtensionPropertiesTestData {
         void setAge(int age);
     }
 
+    public interface PublicUser {
+        public String getName();
+        public void setName(String name);
+
+        public int getAge();
+        public void setAge(int age);
+    }
+
+
+    public interface ReadOnlyUser {
+        String getName();
+        int getAge();
+    }
+
+    public interface WriteOnlyUser {
+        void setName(String name);
+        void setAge(int age);
+    }
+
+    public interface MixedAccessUser {
+        String getName();
+        void setName(String name);
+
+        int getAge();
+    }
+
+    public interface MixedAccessUser2 {
+        String getName();
+        void setName(String name);
+
+        void setAge(int age);
+    }
+
+    public interface SingleGetterUser {
+        String getName();
+    }
+
+    public interface SingleSetterUser {
+        void setName(String name);
+    }
+
+    public interface SinglePropertyUser {
+        String getName();
+        void setName(String name);
+    }
+
+    class Javadoced {
+        /**
+         * Interface representing a user with both getter and setter methods for name and age.
+         */
+        public interface User {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+
+            /**
+             * Gets the age of the user.
+             * @return the age of the user
+             */
+            int getAge();
+
+            /**
+             * Sets the age of the user.
+             * @param age the age to set
+             */
+            void setAge(int age);
+        }
+
+        /**
+         * Interface representing a read-only user with getter methods for name and age.
+         */
+        public interface ReadOnlyUser {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+
+            /**
+             * Gets the age of the user.
+             * @return the age of the user
+             */
+            int getAge();
+        }
+
+        /**
+         * Interface representing a write-only user with setter methods for name and age.
+         */
+        public interface WriteOnlyUser {
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+
+            /**
+             * Sets the age of the user.
+             * @param age the age to set
+             */
+            void setAge(int age);
+        }
+
+        /**
+         * Interface representing a user with mixed access: both getter and setter for name, and getter only for age.
+         */
+        public interface MixedAccessUser {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+
+            /**
+             * Gets the age of the user.
+             * @return the age of the user
+             */
+            int getAge();
+        }
+
+        /**
+         * Interface representing a user with mixed access: both getter and setter for name, and setter only for age.
+         */
+        public interface MixedAccessUser2 {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+
+            /**
+             * Sets the age of the user.
+             * @param age the age to set
+             */
+            void setAge(int age);
+        }
+
+        /**
+         * Interface representing a user with a single getter method for name.
+         */
+        public interface SingleGetterUser {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+        }
+
+        /**
+         * Interface representing a user with a single setter method for name.
+         */
+        public interface SingleSetterUser {
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+        }
+
+        /**
+         * Interface representing a user with both getter and setter methods for name.
+         */
+        public interface SinglePropertyUser {
+            /**
+             * Gets the name of the user.
+             * @return the name of the user
+             */
+            String getName();
+
+            /**
+             * Sets the name of the user.
+             * @param name the name to set
+             */
+            void setName(String name);
+        }
+    }
 
     /**
      public interface User {
-     property String name { get; set; }
-     property int age { get; }
+        property String name { get; set; }
+        property int age { get; }
+     }
+
+     public interface User {
+        String name { get; set; }
+        int age { get; }
      }
 
 
      public interface User {
-     String name { get; set; }
-     int age { get; }
+        @Getter @Setter String name;
+        @Getter int age;
      }
-
-
-     public interface User {
-    @Getter @Setter String name;
-    @Getter int age;
-    }
      */
 }
