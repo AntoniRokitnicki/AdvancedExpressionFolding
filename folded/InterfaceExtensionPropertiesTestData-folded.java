2d49c1ef0eafa75f0a95b7c7df60634926059773
Antoni <antoni2work@gmail.com>
2024-08-12 17:58:38 +0200

    [interfaceExtensionProperties] javadoc support ...

--- folded/InterfaceExtensionPropertiesTestData-folded.java
+++ folded/InterfaceExtensionPropertiesTestData-folded.java
@@ -65,148 +65,148 @@ public class InterfaceExtensionPropertiesTestData {
          * Interface representing a user with both getter and setter methods for name and age.
          */
         public interface User {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
 
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
 
-           @Getter /**
+            /**
              * Gets the age of the user.
              * @return the age of the user
              */
-            int age;
+           @Getter int age;
 
-           @Setter /**
+            /**
              * Sets the age of the user.
              * @param age the age to set
              */
-            int age;
+           @Setter int age;
         }
 
         /**
          * Interface representing a read-only user with getter methods for name and age.
          */
         public interface ReadOnlyUser {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
 
-           @Getter /**
+            /**
              * Gets the age of the user.
              * @return the age of the user
              */
-            int age;
+           @Getter int age;
         }
 
         /**
          * Interface representing a write-only user with setter methods for name and age.
          */
         public interface WriteOnlyUser {
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
 
-           @Setter /**
+            /**
              * Sets the age of the user.
              * @param age the age to set
              */
-            int age;
+           @Setter int age;
         }
 
         /**
          * Interface representing a user with mixed access: both getter and setter for name, and getter only for age.
          */
         public interface MixedAccessUser {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
 
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
 
-           @Getter /**
+            /**
              * Gets the age of the user.
              * @return the age of the user
              */
-            int age;
+           @Getter int age;
         }
 
         /**
          * Interface representing a user with mixed access: both getter and setter for name, and setter only for age.
          */
         public interface MixedAccessUser2 {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
 
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
 
-           @Setter /**
+            /**
              * Sets the age of the user.
              * @param age the age to set
              */
-            int age;
+           @Setter int age;
         }
 
         /**
          * Interface representing a user with a single getter method for name.
          */
         public interface SingleGetterUser {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
         }
 
         /**
          * Interface representing a user with a single setter method for name.
          */
         public interface SingleSetterUser {
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
         }
 
         /**
          * Interface representing a user with both getter and setter methods for name.
          */
         public interface SinglePropertyUser {
-           @Getter /**
+            /**
              * Gets the name of the user.
              * @return the name of the user
              */
-            String name;
+           @Getter String name;
 
-           @Setter /**
+            /**
              * Sets the name of the user.
              * @param name the name to set
              */
-            String name;
+           @Setter String name;
         }
     }
 
