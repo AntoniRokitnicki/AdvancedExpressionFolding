9e8f56a6ff9f1a3fe82ffccf5c416c24a4ea6a23
Antoni <antoni2work@gmail.com>
2024-10-28 20:16:52 +0100

    [summaryParentOverride] init

---
+++ examples/data/SummaryParentOverrideTestData.java
@@ -0,0 +1,77 @@
+package data;
+
+public class SummaryParentOverrideTestData extends ParentClass implements FirstInterface, SecondInterface {
+
+    @Override
+    public void interfaceMethodOne() {
+        System.out.println("Implementation of Interface Method One");
+    }
+
+    @Override
+    public void interfaceMethodTwo() {
+        System.out.println("Implementation of Interface Method Two");
+    }
+
+    @Override
+    public void sharedMethod() {
+        System.out.println("Overridden Shared Method");
+    }
+
+    @Override
+    public void grandparentMethod() {
+        System.out.println("Overridden Grandparent Method in SummaryParentOverrideTestData");
+    }
+
+    public void additionalMethodOne() {
+        System.out.println("Additional method one");
+    }
+
+    public void additionalMethodTwo() {
+        System.out.println("Additional method two");
+    }
+
+    public void additionalMethodThree() {
+        System.out.println("Additional method three");
+    }
+
+    public static void main(String[] args) {
+        SummaryParentOverrideTestData testData = new SummaryParentOverrideTestData();
+        testData.interfaceMethodOne();
+        testData.interfaceMethodTwo();
+        testData.sharedMethod();
+        testData.grandparentMethod();
+        testData.additionalMethodOne();
+        testData.additionalMethodTwo();
+        testData.additionalMethodThree();
+    }
+}
+
+class GrandparentClass {
+    public void grandparentMethod() {
+        System.out.println("Grandparent Method");
+    }
+
+    public void sharedMethod() {
+        System.out.println("Shared method from Grandparent");
+    }
+}
+
+class ParentClass extends GrandparentClass {
+    @Override
+    public void grandparentMethod() {
+        System.out.println("Overridden Grandparent Method in Parent");
+    }
+
+    public void parentMethod() {
+        System.out.println("Parent Method");
+    }
+}
+
+interface FirstInterface {
+    void interfaceMethodOne();
+    void sharedMethod();
+}
+
+interface SecondInterface {
+    void interfaceMethodTwo();
+}
