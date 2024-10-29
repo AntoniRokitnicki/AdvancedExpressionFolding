8751f6885d99e961bacd3a311c710342d65143bc
Antoni <antoni2work@gmail.com>
2024-10-29 22:02:21 +0100

    [summaryParentOverride] implementation finished

--- src/com/intellij/advancedExpressionFolding/extension/lombok/SummaryParentOverrideExt.kt
+++ src/com/intellij/advancedExpressionFolding/extension/lombok/SummaryParentOverrideExt.kt
@@ -4,8 +4,6 @@ import com.intellij.advancedExpressionFolding.expression.Expression
 import com.intellij.advancedExpressionFolding.extension.*
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
-import com.intellij.psi.impl.search.JavaAllOverridingMethodsSearcher
-import com.intellij.psi.impl.search.MethodSuperSearcher
 
 private data class ReferenceWithMethods(
     val element: PsiElement,
@@ -18,27 +16,28 @@ object SummaryParentOverrideExt : BaseExtension() {
         summaryParentOverride.on() ?: return null
         val className = this.name ?: return null
 
-        sequenceOf(this.extendsList, this.implementsList).filterNotNull().mapNotNull { parent ->
-            val a = parent.referencedTypes
-            parent.referenceElements.zip(a).mapNotNull { (refElement, type) ->
-                val c = type.resolve()
+        sequenceOf(this.extendsList, this.implementsList).filterNotNull().map { parent ->
+            parent.referenceElements.zip(parent.referencedTypes).mapNotNull { (refElement, type) ->
                 refElement.referenceNameElement?.let { element ->
-                    val overriddenMethods = c?.methods?.filter { method ->
-                        this.findMethodBySignature(method, false) != null
-                    }?.map { it.name } ?: emptyList()
+                    val overriddenMethods = type.resolve()?.methods?.filter { method ->
+                        findMethodBySignature(method, false) != null
+                    }?.map {
+                        it.name
+                    } ?: emptyList()
                     ReferenceWithMethods(element, overriddenMethods)
                 }
             }
         }.flatten().toList().takeIf {
             it.isNotEmpty()
         }?.mapNotNull {
-            val size = it.methods.size
-            it.element.exprOnLastChar("($size-${asString(it)})")
+            it.element.exprOnLastChar("(${asCount(it)}-${asString(it)})")
         }?.let {
             return it.exprWrap(this)
         }
         return null
     }
+
+    private fun asCount(it: ReferenceWithMethods) = it.methods.size
 
     private fun asString(it: ReferenceWithMethods) = it.methods.joinToString(", ")
 }
