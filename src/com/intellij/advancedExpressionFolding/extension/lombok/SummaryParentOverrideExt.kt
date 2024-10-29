cbdcabe3ed9395a6da7b602eae5805952c039311
Antoni <antoni2work@gmail.com>
2024-10-29 21:51:08 +0100

    [summaryParentOverride] search for overrides

--- src/com/intellij/advancedExpressionFolding/extension/lombok/SummaryParentOverrideExt.kt
+++ src/com/intellij/advancedExpressionFolding/extension/lombok/SummaryParentOverrideExt.kt
@@ -3,16 +3,21 @@ package com.intellij.advancedExpressionFolding.extension.lombok
 import com.intellij.advancedExpressionFolding.expression.Expression
 import com.intellij.advancedExpressionFolding.extension.*
 import com.intellij.psi.PsiClass
+import com.intellij.psi.impl.search.JavaAllOverridingMethodsSearcher
+import com.intellij.psi.impl.search.MethodSuperSearcher
 
 object SummaryParentOverrideExt : BaseExtension() {
 
     fun PsiClass.addParentSummary(): Expression? {
         summaryParentOverride.on() ?: return null
-        //val className = this.name ?: return null
+        val className = this.name ?: return null
 
         sequenceOf(this.extendsList, this.implementsList).filterNotNull().mapNotNull { parent ->
             val a = parent.referencedTypes
             parent.referenceElements.mapNotNull {
+                //TODO: not first but matching, maybe use zip here?
+                val c= a.first().resolve()
+                //TODO: use JavaAllOverridingMethodsSearcher or something else to find all methods overridden or implemented(in case of interfaces) in current class(stored in this) that are taken from c or its children and print it out here
                 it?.referenceNameElement
             }
         }.flatten().toList().takeIf {
