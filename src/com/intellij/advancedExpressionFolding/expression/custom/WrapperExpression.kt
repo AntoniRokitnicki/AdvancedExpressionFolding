adfe62edf2fe009d3a807afaafd7f6774f0f48ee
Antoni <antoni2work@gmail.com>
2025-04-06 11:30:39 +0200

    cleanup WrapperExpression

--- src/com/intellij/advancedExpressionFolding/expression/custom/WrapperExpression.kt
+++ src/com/intellij/advancedExpressionFolding/expression/custom/WrapperExpression.kt
@@ -9,11 +9,6 @@ import com.intellij.psi.PsiElement
 open class WrapperExpression(element: PsiElement, textRange: TextRange = element.textRange, val chain : List<Expression> = emptyList(), private val nested: Boolean = true) :
     AbstractMultiExpression(element, textRange, *chain.toTypedArray()) {
 
-    fun getChanges(): List<Expression> {
-        return chain
-            //.filter {it !is HideExpression}
-    }
-
     override fun isNested() = nested
 
     override fun wrapElement(element: PsiElement, parentGroup: FoldingGroup): MutableList<FoldingDescriptor> =
