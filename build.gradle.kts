bcd0be4f0415fffecfa1b42228c9724bf30515a3
Antoni <antoni2work@gmail.com>
2025-04-16 18:52:50 +0200

    fix java version to work with older versions of IDE

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ version = properties("pluginVersion").get()
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
     jvmToolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(17) // to support 2023.3.8 and 2024.1.7
         @Suppress("UnstableApiUsage")
         vendor = JvmVendorSpec.JETBRAINS
     }
