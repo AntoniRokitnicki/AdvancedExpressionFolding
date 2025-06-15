e4508485964235fc1bca051d5613c445f3d1f3bb
Antoni <antoni2work@gmail.com>
2025-06-15 22:39:07 +0200

    remove deprecation warning

--- src/com/intellij/advancedExpressionFolding/settings/CheckboxesProvider.kt
+++ src/com/intellij/advancedExpressionFolding/settings/CheckboxesProvider.kt
@@ -14,6 +14,7 @@ import kotlin.reflect.KMutableProperty0
 
 abstract class CheckboxesProvider {
 
+    @Suppress("DEPRECATION")
     fun Panel.initialize(state: State) {
         registerCheckbox(state::getSetExpressionsCollapse, "Getters and setters as properties") {
             example("GetterSetterTestData.java")
