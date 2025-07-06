1a3e91e7b808a7a9665652fac923ca92ee8f1741
Antoni <antoni2work@gmail.com>
2025-07-06 22:01:59 +0200

    add button to apply folded text color based on current theme (blue/navy) in settings

---
+++ src/com/intellij/advancedExpressionFolding/UpdateFoldedTextColorsAction.kt
@@ -0,0 +1,30 @@
+package com.intellij.advancedExpressionFolding
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.editor.colors.EditorColors
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.ui.JBColor
+import java.awt.Color.decode
+
+class UpdateFoldedTextColorsAction : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val scheme = EditorColorsManager.getInstance().globalScheme
+        val textAttributes = scheme.getAttributes(EditorColors.FOLDED_TEXT_ATTRIBUTES)
+        val foregroundColor = if (!JBColor.isBright()) {
+            decode("#7ca0bb")
+        } else {
+            decode("#000091")
+        }
+        val backgroundColor = null
+        val foldedAttributes = TextAttributes(
+            foregroundColor,
+            backgroundColor,
+            textAttributes.effectColor,
+            textAttributes.effectType,
+            textAttributes.fontType
+        )
+        scheme.setAttributes(EditorColors.FOLDED_TEXT_ATTRIBUTES, foldedAttributes)
+    }
+}
--- src/com/intellij/advancedExpressionFolding/settings/SettingsConfigurable.kt
+++ src/com/intellij/advancedExpressionFolding/settings/SettingsConfigurable.kt
@@ -1,12 +1,17 @@
 package com.intellij.advancedExpressionFolding.settings
 
 import com.intellij.advancedExpressionFolding.AdvancedExpressionFoldingSettings
+import com.intellij.advancedExpressionFolding.UpdateFoldedTextColorsAction
 import com.intellij.application.options.CodeStyle
 import com.intellij.application.options.editor.EditorOptionsProvider
 import com.intellij.icons.AllIcons
 import com.intellij.ide.BrowserUtil
+import com.intellij.ide.DataManager
 import com.intellij.ide.HelpTooltip
 import com.intellij.ide.impl.ProjectUtil
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.ActionUiKind
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
@@ -14,12 +19,15 @@ import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.encoding.EncodingProjectManager
+import com.intellij.ui.JBColor
 import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.dsl.builder.panel
+import java.awt.Color.decode
 import java.awt.FlowLayout
 import java.net.URI
+import javax.swing.JButton
 import javax.swing.JPanel
 import kotlin.reflect.KMutableProperty0
 
@@ -90,6 +98,26 @@ class SettingsConfigurable : EditorOptionsProvider, CheckboxesProvider() {
     }
 
     override fun createComponent() = panel {
+        row {
+            val themeColor = if (!JBColor.isBright()) decode("#7ca0bb") else decode("#000091")
+            val button =
+                JButton("Apply folded color: ${if (!JBColor.isBright()) "soft blue" else "dark navy"}")
+            button.foreground = themeColor
+            button.addActionListener {
+                val action = UpdateFoldedTextColorsAction()
+                val dataContext = DataManager.getInstance().dataContextFromFocusAsync.blockingGet(200)
+                val event = AnActionEvent.createEvent(
+                    action,
+                    dataContext!!,
+                    action.templatePresentation.clone(),
+                    ActionPlaces.UNKNOWN,
+                    ActionUiKind.NONE,
+                    null
+                )
+                action.actionPerformed(event)
+            }
+            cell(button)
+        }
         row {
             cell(createDownloadExamplesLink())
         }
