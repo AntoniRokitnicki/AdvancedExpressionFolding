ee0529e8a2e963c90aa8cd87706d43410164a593
Antoni <antoni2ee@gmail.com>
2025-10-20 10:40:37 -0700

    Refactor collapsed operations

--- src/com/intellij/advancedExpressionFolding/expression/operation/stream/StreamFilterNotNull.kt
+++ src/com/intellij/advancedExpressionFolding/expression/operation/stream/StreamFilterNotNull.kt
@@ -1,8 +1,7 @@
 package com.intellij.advancedExpressionFolding.expression.operation.stream
 
 import com.intellij.advancedExpressionFolding.expression.Expression
-import com.intellij.advancedExpressionFolding.expression.Operation
-import com.intellij.openapi.editor.Document
+import com.intellij.advancedExpressionFolding.expression.operation.CollapsedOperation
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 
@@ -10,14 +9,8 @@ class StreamFilterNotNull(
     element: PsiElement,
     textRange: TextRange,
     operands: List<Expression>
-) : Operation(element, textRange, ".filterNotNull()", 300, operands) {
-    override fun buildFolding(character: String): String = character
-
+) : CollapsedOperation(element, ".filterNotNull()", operands, textRange) {
     override fun changeOperandsEndOffset(startOffset: Int): Int {
         return startOffset + "Objects::nonNull".length
     }
-
-    override fun isCollapsedByDefault(): Boolean = true
-
-    override fun supportsFoldRegions(document: Document, parent: Expression?): Boolean = true
 }
--- src/com/intellij/advancedExpressionFolding/expression/operation/stream/StringOperation.kt
+++ src/com/intellij/advancedExpressionFolding/expression/operation/stream/StringOperation.kt
@@ -1,28 +1,13 @@
 package com.intellij.advancedExpressionFolding.expression.operation.stream
 
 import com.intellij.advancedExpressionFolding.expression.Expression
-import com.intellij.advancedExpressionFolding.expression.Operation
-import com.intellij.openapi.editor.Document
+import com.intellij.advancedExpressionFolding.expression.operation.CollapsedOperation
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-
 
 open class StringOperation(
     element: PsiElement,
     text: String,
     operands: List<Expression> = emptyList(),
     textRange: TextRange = element.textRange,
-) : Operation(
-    element,
-    textRange,
-    text,
-    300,
-    operands
-) {
-
-    override fun buildFolding(character: String): String = character
-
-    override fun isCollapsedByDefault(): Boolean = true
-
-    override fun supportsFoldRegions(document: Document, parent: Expression?): Boolean = true
-}
+) : CollapsedOperation(element, text, operands, textRange)
