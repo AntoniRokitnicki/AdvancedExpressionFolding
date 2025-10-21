79124cad12473dfb59c8d6918fdc4cd1585a5c15
Antoni <antoni2work@gmail.com>
2025-10-21 14:59:09 -0700

    Refactor settings state into feature groups (#572)

--- src/com/intellij/advancedExpressionFolding/settings/IState.kt
+++ src/com/intellij/advancedExpressionFolding/settings/IState.kt
@@ -1,56 +1,14 @@
 package com.intellij.advancedExpressionFolding.settings
 
-interface IState {
-    val concatenationExpressionsCollapse: Boolean
-    val slicingExpressionsCollapse: Boolean
-    val comparingExpressionsCollapse: Boolean
-    val comparingLocalDatesCollapse: Boolean
-    val localDateLiteralCollapse: Boolean
-    val localDateLiteralPostfixCollapse: Boolean
-    val getExpressionsCollapse: Boolean
-    val rangeExpressionsCollapse: Boolean
-    val checkExpressionsCollapse: Boolean
-    val castExpressionsCollapse: Boolean
-    val varExpressionsCollapse: Boolean
-    val getSetExpressionsCollapse: Boolean
-    val controlFlowSingleStatementCodeBlockCollapse: Boolean
-    val compactControlFlowSyntaxCollapse: Boolean
-    val controlFlowMultiStatementCodeBlockCollapse: Boolean
-    val semicolonsCollapse: Boolean
-    val assertsCollapse: Boolean
-
-    val optional: Boolean
-    val streamSpread: Boolean
-    val lombok: Boolean
-    val fieldShift: Boolean
-    val kotlinQuickReturn: Boolean
-    val ifNullSafe: Boolean
-
-    val logFolding: Boolean
-    val logFoldingTextBlocks: Boolean
-
-    val destructuring: Boolean
-    val println: Boolean
-    val const: Boolean
-    val nullable: Boolean
-    val finalRemoval: Boolean
-    val finalEmoji: Boolean
-    val lombokDirtyOff: Boolean
-
-    val expressionFunc: Boolean
-    val dynamic: Boolean
-    val arithmeticExpressions: Boolean
-    val emojify: Boolean
-    val interfaceExtensionProperties: Boolean
-    val patternMatchingInstanceof: Boolean
-    val summaryParentOverride: Boolean
-    val constructorReferenceNotation: Boolean
-    val methodDefaultParameters: Boolean
-    val lombokPatternOff: String?
-    val overrideHide: Boolean
-    val suppressWarningsHide: Boolean
-    val pseudoAnnotations: Boolean
-    // NEW OPTION VAL
-
-    var experimental: Boolean
-}
+interface IState :
+    ILombokState,
+    ILogFoldingState,
+    IControlFlowState,
+    IDateOperationsState,
+    IKotlinLanguageState,
+    ICollectionsStreamsState,
+    IExpressionCollapseState,
+    IGlobalSettingsState,
+    IEmojiVisibilityState,
+    IHidingSuppressionState,
+    IUnclassifiedFeatureState
