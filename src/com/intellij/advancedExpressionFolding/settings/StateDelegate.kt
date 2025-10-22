de8288feb32cf67758a729b0dca3a9b1f00c3a15
Antoni <antoni2ee@gmail.com>
2025-10-22 10:09:59 -0700

    Use delegated control-flow state without exposing backing field

--- src/com/intellij/advancedExpressionFolding/settings/StateDelegate.kt
+++ src/com/intellij/advancedExpressionFolding/settings/StateDelegate.kt
@@ -1,3 +1,6 @@
 package com.intellij.advancedExpressionFolding.settings
 
-open class StateDelegate(private val state: AdvancedExpressionFoldingSettings.State = AdvancedExpressionFoldingSettings.Companion.getInstance().state) : IState by state
+open class StateDelegate(
+    private val state: AdvancedExpressionFoldingSettings.State =
+        AdvancedExpressionFoldingSettings.getInstance().state,
+) : IState by state
