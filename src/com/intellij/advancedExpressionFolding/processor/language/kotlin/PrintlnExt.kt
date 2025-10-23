ee7cdb340a90a502d7906837891cf83b96120d9b
Antoni <antoni2ee@gmail.com>
2025-10-23 07:36:47 -0700

    Refine Kotlin println extension state

--- src/com/intellij/advancedExpressionFolding/processor/language/kotlin/PrintlnExt.kt
+++ src/com/intellij/advancedExpressionFolding/processor/language/kotlin/PrintlnExt.kt
@@ -2,15 +2,16 @@ package com.intellij.advancedExpressionFolding.processor.language.kotlin
 
 import com.intellij.advancedExpressionFolding.expression.Expression
 import com.intellij.advancedExpressionFolding.expression.semantic.kotlin.PrintlnExpression
-import com.intellij.advancedExpressionFolding.processor.core.BaseExtension
 import com.intellij.advancedExpressionFolding.processor.end
 import com.intellij.advancedExpressionFolding.processor.equalsIgnoreSpaces
 import com.intellij.advancedExpressionFolding.processor.isWhitespace
 import com.intellij.advancedExpressionFolding.processor.start
+import com.intellij.advancedExpressionFolding.settings.AdvancedExpressionFoldingSettings
+import com.intellij.advancedExpressionFolding.settings.IKotlinLanguageState
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiMethodCallExpression
 
-object PrintlnExt : BaseExtension() {
+object PrintlnExt : IKotlinLanguageState by AdvancedExpressionFoldingSettings.State() {
 
     fun createExpression(
         element: PsiMethodCallExpression,
