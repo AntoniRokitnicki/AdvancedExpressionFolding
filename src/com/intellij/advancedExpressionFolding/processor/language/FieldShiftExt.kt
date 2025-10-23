dedc5f503e5e643de0780117df48461120a3ac26
Antoni <antoni2ee@gmail.com>
2025-10-23 10:09:11 -0700

    Delegate FieldShiftExt state to settings

--- src/com/intellij/advancedExpressionFolding/processor/language/FieldShiftExt.kt
+++ src/com/intellij/advancedExpressionFolding/processor/language/FieldShiftExt.kt
@@ -8,13 +8,14 @@ import com.intellij.advancedExpressionFolding.expression.property.INameable
 import com.intellij.advancedExpressionFolding.expression.semantic.BuilderShiftExpression
 import com.intellij.advancedExpressionFolding.expression.semantic.kotlin.CheckNotNullExpression
 import com.intellij.advancedExpressionFolding.processor.*
-import com.intellij.advancedExpressionFolding.processor.core.BaseExtension
 import com.intellij.advancedExpressionFolding.processor.core.BuildExpressionExt
 import com.intellij.advancedExpressionFolding.processor.core.getAnyExpression
+import com.intellij.advancedExpressionFolding.settings.AdvancedExpressionFoldingSettings
+import com.intellij.advancedExpressionFolding.settings.IExpressionCollapseState
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.*
 
-object FieldShiftExt : BaseExtension() {
+object FieldShiftExt : IExpressionCollapseState by AdvancedExpressionFoldingSettings.State()() {
     private const val FIELD_SHIFT = "<<"
 
     @JvmStatic
