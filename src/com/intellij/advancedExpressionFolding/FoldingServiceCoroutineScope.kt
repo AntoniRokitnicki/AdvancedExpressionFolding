991594aa423b9558dd84c132faabe7a9c221b358
Antoni <antoni2work@gmail.com>
2025-10-28 20:28:52 +0100

    1 compatibility problem Advanced Java Folding 2 (Fork) 761.678.268-canary is binary incompatible with IntelliJ IDEA Ultimate IU-233.15619.7 due to the following problem Method not found (1) Invocation of unresolved method ServicesKt.serviceNotFoundError(...) (1) Method FoldingService.clearAllKeys(Project) contains an invokestatic instruction referencing an unresolved method ServicesKt.serviceNotFoundError(...). This can lead to NoSuchMethodError exception at runtime.

--- src/com/intellij/advancedExpressionFolding/FoldingServiceCoroutineScope.kt
+++ src/com/intellij/advancedExpressionFolding/FoldingServiceCoroutineScope.kt
@@ -2,19 +2,24 @@ package com.intellij.advancedExpressionFolding
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlin.coroutines.CoroutineContext
 
-@Service(Service.Level.PROJECT)
-class FoldingServiceCoroutineScope(project: Project) : CoroutineScope, Disposable {
+@Service
+class FoldingServiceCoroutineScope : CoroutineScope, Disposable {
     private val job = SupervisorJob()
 
     override val coroutineContext: CoroutineContext = job + Dispatchers.Default
 
     override fun dispose() {
         job.cancel()
+    }
+
+    companion object {
+        fun get() = service<FoldingServiceCoroutineScope>()
     }
 }
