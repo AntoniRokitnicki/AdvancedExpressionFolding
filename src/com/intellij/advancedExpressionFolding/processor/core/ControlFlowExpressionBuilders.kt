6a36581ab9da33195000b019550d0079990aa1c5
Antoni <antoni2ee@gmail.com>
2025-11-07 19:45:07 +0100

    Refactor expression builder registration

--- src/com/intellij/advancedExpressionFolding/processor/core/ControlFlowExpressionBuilders.kt
+++ src/com/intellij/advancedExpressionFolding/processor/core/ControlFlowExpressionBuilders.kt
@@ -1,12 +1,10 @@
 package com.intellij.advancedExpressionFolding.processor.core
 
-import com.intellij.advancedExpressionFolding.expression.Expression
 import com.intellij.advancedExpressionFolding.expression.controlflow.CompactControlFlowExpression
 import com.intellij.advancedExpressionFolding.processor.controlflow.ForStatementExpressionExt
 import com.intellij.advancedExpressionFolding.processor.controlflow.IfExt
 import com.intellij.advancedExpressionFolding.processor.controlflow.LoopExt
 import com.intellij.advancedExpressionFolding.processor.controlflow.PsiTryStatementExt
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiCatchSection
 import com.intellij.psi.PsiDoWhileStatement
@@ -17,54 +15,39 @@ import com.intellij.psi.PsiSwitchStatement
 import com.intellij.psi.PsiTryStatement
 import com.intellij.psi.PsiWhileStatement
 
-class ForStatementBuilder : BuildExpression<PsiForStatement>(PsiForStatement::class.java) {
-    override fun buildExpression(element: PsiForStatement, document: Document, synthetic: Boolean) =
+internal val controlFlowExpressionBuilders = listOf(
+    registerBuilder<PsiForStatement> { element, document, _ ->
         ForStatementExpressionExt.getForStatementExpression(element, document)
-}
-
-class ForEachStatementBuilder : BuildExpression<PsiForeachStatement>(PsiForeachStatement::class.java) {
-    override fun buildExpression(element: PsiForeachStatement, document: Document, synthetic: Boolean): Expression? =
+    },
+    registerBuilder<PsiForeachStatement> { element, _, _ ->
         LoopExt.getForEachStatementExpression(element)
-}
-
-class IfStatementBuilder : BuildExpression<PsiIfStatement>(PsiIfStatement::class.java) {
-    override fun buildExpression(element: PsiIfStatement, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiIfStatement> { element, document, _ ->
         IfExt.getIfExpression(element, document)
-}
-
-class WhileStatementBuilder : BuildExpression<PsiWhileStatement>(PsiWhileStatement::class.java) {
-    override fun buildExpression(element: PsiWhileStatement, document: Document, synthetic: Boolean): Expression? =
+    },
+    registerBuilder<PsiWhileStatement> { element, _, _ ->
         LoopExt.getWhileStatement(element)
-}
-
-class DoWhileStatementBuilder : BuildExpression<PsiDoWhileStatement>(PsiDoWhileStatement::class.java) {
-    override fun buildExpression(element: PsiDoWhileStatement, document: Document, synthetic: Boolean): Expression? =
+    },
+    registerBuilder<PsiDoWhileStatement> { element, _, _ ->
         LoopExt.getDoWhileStatement(element)
-}
-
-class SwitchStatementBuilder : BuildExpression<PsiSwitchStatement>(PsiSwitchStatement::class.java) {
-    override fun buildExpression(element: PsiSwitchStatement, document: Document, synthetic: Boolean): Expression? =
+    },
+    registerBuilder<PsiSwitchStatement> { element, _, _ ->
         IfExt.getSwitchStatement(element)
-}
-
-class TryStatementBuilder : BuildExpression<PsiTryStatement>(PsiTryStatement::class.java) {
-    override fun buildExpression(element: PsiTryStatement, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiTryStatement> { element, _, _ ->
         PsiTryStatementExt.createExpression(element)
-}
-
-class CatchSectionBuilder : BuildExpression<PsiCatchSection>(PsiCatchSection::class.java) {
-    override fun checkConditions(element: PsiCatchSection) =
+    },
+    registerBuilder<PsiCatchSection>(predicate = {
         compactControlFlowSyntaxCollapse &&
-                element.parameter != null &&
-                element.lParenth != null &&
-                element.rParenth != null
-
-    override fun buildExpression(element: PsiCatchSection, document: Document, synthetic: Boolean): Expression? {
-        val l = element.lParenth ?: return null
-        val r = element.rParenth ?: return null
-        return CompactControlFlowExpression(
+            it.parameter != null &&
+            it.lParenth != null &&
+            it.rParenth != null
+    }) { element, _, _ ->
+        val l = element.lParenth ?: return@registerBuilder null
+        val r = element.rParenth ?: return@registerBuilder null
+        CompactControlFlowExpression(
             element,
             TextRange.create(l.textRange.startOffset, r.textRange.endOffset)
         )
     }
-}
+)
--- src/com/intellij/advancedExpressionFolding/processor/core/DeclarationBuilders.kt
+++ src/com/intellij/advancedExpressionFolding/processor/core/DeclarationBuilders.kt
@@ -6,7 +6,6 @@ import com.intellij.advancedExpressionFolding.processor.declaration.PsiDeclarati
 import com.intellij.advancedExpressionFolding.processor.declaration.PsiFieldExt
 import com.intellij.advancedExpressionFolding.processor.declaration.PsiMethodExt
 import com.intellij.advancedExpressionFolding.processor.declaration.PsiVariableExt
-import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiCodeBlock
 import com.intellij.psi.PsiDeclarationStatement
@@ -17,45 +16,32 @@ import com.intellij.psi.PsiParameter
 import com.intellij.psi.PsiRecordComponent
 import com.intellij.psi.PsiVariable
 
-class DeclarationStatementBuilder : BuildExpression<PsiDeclarationStatement>(PsiDeclarationStatement::class.java) {
-    override fun buildExpression(element: PsiDeclarationStatement, document: Document, synthetic: Boolean) =
+internal val declarationExpressionBuilders = listOf(
+    registerBuilder<PsiDeclarationStatement> { element, _, _ ->
         PsiDeclarationStatementExt.createExpression(element)
-}
-
-class VariableBuilder : BuildExpression<PsiVariable>(PsiVariable::class.java) {
-    override fun checkConditions(element: PsiVariable) = varExpressionsCollapse &&
-            (element.parent is PsiDeclarationStatement || element.parent is PsiForeachStatement)
-
-    override fun buildExpression(element: PsiVariable, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiVariable>(predicate = {
+        varExpressionsCollapse &&
+            (it.parent is PsiDeclarationStatement || it.parent is PsiForeachStatement)
+    }) { element, _, _ ->
         PsiVariableExt.getVariableDeclaration(element)
-}
-
-class CodeBlockBuilder : BuildExpression<PsiCodeBlock>(PsiCodeBlock::class.java) {
-    override fun buildExpression(element: PsiCodeBlock, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiCodeBlock> { element, _, _ ->
         PsiCodeBlockExt.getCodeBlockExpression(element)
-}
-
-class ClassBuilder : BuildExpression<PsiClass>(PsiClass::class.java) {
-    override fun buildExpression(element: PsiClass, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiClass> { element, _, _ ->
         PsiClassExt.createExpression(element)
-}
-
-class FieldBuilder : BuildExpression<PsiField>(PsiField::class.java) {
-    override fun buildExpression(element: PsiField, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiField> { element, document, _ ->
         PsiFieldExt.createExpression(element, document)
-}
-
-class ParameterBuilder : BuildExpression<PsiParameter>(PsiParameter::class.java) {
-    override fun buildExpression(element: PsiParameter, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiParameter> { element, document, _ ->
         PsiMethodExt.createExpression(element, document)
-}
-
-class RecordComponentBuilder : BuildExpression<PsiRecordComponent>(PsiRecordComponent::class.java) {
-    override fun buildExpression(element: PsiRecordComponent, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiRecordComponent> { element, _, _ ->
         PsiFieldExt.createExpression(element)
-}
-
-class MethodBuilder : BuildExpression<PsiMethod>(PsiMethod::class.java) {
-    override fun buildExpression(element: PsiMethod, document: Document, synthetic: Boolean) =
+    },
+    registerBuilder<PsiMethod> { element, document, _ ->
         PsiMethodExt.createExpression(element, document)
-}
+    }
+)
